2deb674d944272e6b67d1337fcc68350e84fd2cc
Nwafor6 <nwaforglory6@gmail.com>
2023-06-11 18:02:56 +0100

    updated wsgi

--- project/wsgi.py
+++ project/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except:
+    app = get_wsgi_application()
